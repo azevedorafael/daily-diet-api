1048a1a6f831b822772accc61e89e404c0cd5a3c
azevedorafael <rafaelsilva.fatec@gmail.com>
2023-05-09 22:06:04 -0300

    add meals routes

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -7,6 +7,10 @@ import { checkUserIdExists } from '../middlewares/check-user-id-exists'
 const TABLE_NAME = 'meals'
 
 export async function mealsRoutes(app: FastifyInstance) {
+  // app.addHook('preHandler', async (request, reply) => {
+  //   console.log(`[${request.method}] ${request.url}`)
+  // })
+
   app.get(
     '/',
     {
@@ -18,6 +22,72 @@ export async function mealsRoutes(app: FastifyInstance) {
       const meals = await knex(TABLE_NAME).where('user_id', userId).select()
 
       return { meals }
+    },
+  )
+
+  app.get(
+    '/:id',
+    {
+      preHandler: [checkUserIdExists],
+    },
+    async (request, reply) => {
+      const getMealParamsSchema = z.object({
+        id: z.string().uuid(),
+      })
+      const { id } = getMealParamsSchema.parse(request.params)
+      const { userId } = request.cookies
+
+      const meal = await knex(TABLE_NAME)
+        .select()
+        .where('id', id)
+        .and.where('user_id', userId)
+
+      return { meal }
+    },
+  )
+
+  app.get(
+    '/metrics',
+    {
+      preHandler: [checkUserIdExists],
+    },
+    async (request, reply) => {
+      const { userId } = request.cookies
+      const totalMealsAcount: number = Number(
+        (await knex(TABLE_NAME).where('user_id', userId).count())[0][
+          'count(*)'
+        ],
+      )
+      const totalMealsAcountWithinDiet = Number(
+        (
+          await knex(TABLE_NAME)
+            .where('user_id', userId)
+            .and.where('within_diet', true)
+            .count()
+        )[0]['count(*)'],
+      )
+
+      let bestSequenceWithinDiet = 0
+      const sequencesInsideTheDiet = await knex(TABLE_NAME)
+        .where('user_id', userId)
+        .and.where('within_diet', true)
+        .groupBy('created_at')
+        .count()
+
+      sequencesInsideTheDiet.forEach((element) => {
+        const mealsInsideTheDiet = Number(element['count(*)'])
+        if (mealsInsideTheDiet > bestSequenceWithinDiet) {
+          bestSequenceWithinDiet = mealsInsideTheDiet
+        }
+      })
+
+      return {
+        totalMealsAcount,
+        totalMealsAcountWithinDiet,
+        totalMealsAcountOutOfDiet:
+          totalMealsAcount - totalMealsAcountWithinDiet,
+        bestSequenceWithinDiet,
+      }
     },
   )
 
@@ -55,4 +125,60 @@ export async function mealsRoutes(app: FastifyInstance) {
 
     reply.status(201).send()
   })
+
+  app.put(
+    '/:id',
+    {
+      preHandler: [checkUserIdExists],
+    },
+    async (request, reply) => {
+      const getMealParamsSchema = z.object({
+        id: z.string().uuid(),
+      })
+      const { id } = getMealParamsSchema.parse(request.params)
+      const createUserBodySchema = z.object({
+        name: z.string(),
+        description: z.string(),
+        dateTime: z.string().datetime(),
+        withinDiet: z.boolean(),
+      })
+
+      const { name, description, dateTime, withinDiet } =
+        createUserBodySchema.parse(request.body)
+      const { userId } = request.cookies
+
+      await knex(TABLE_NAME)
+        .update({
+          name,
+          description,
+          created_at: dateTime,
+          within_diet: withinDiet,
+        })
+        .where('id', id)
+        .and.where('user_id', userId)
+
+      reply.status(201).send()
+    },
+  )
+
+  app.delete(
+    '/:id',
+    {
+      preHandler: [checkUserIdExists],
+    },
+    async (request, reply) => {
+      const getMealParamsSchema = z.object({
+        id: z.string().uuid(),
+      })
+      const { id } = getMealParamsSchema.parse(request.params)
+      const { userId } = request.cookies
+
+      await knex(TABLE_NAME)
+        .delete()
+        .where('id', id)
+        .and.where('user_id', userId)
+
+      reply.status(201).send()
+    },
+  )
 }
